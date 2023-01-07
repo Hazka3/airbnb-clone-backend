9aa5d7932bf62eb883eb163dad64e1b22c6b6ade
Hazka <94900388+Hazka3@users.noreply.github.com>
2023-01-07 15:22:21 +0900

    22.01.07.001

---
+++ users/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+        JP = ("jp", "Japan")
+
+    class CurrencyChoices(models.TextChoices):
+        KRW = "won", "Korean Won"
+        USD = "usd", "US Dollar"
+        JPY = "jpy", "Japan Yen"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    avatar = models.ImageField(blank=True)
+    is_host = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
